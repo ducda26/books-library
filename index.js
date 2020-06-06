00cec30010a0d0b0803fdde5e39dca90d68fe344
Đức Anh <you@example.com>
2020-06-06 16:22:47 +0700

    them chuc nang tim kiem

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ var port = 1000
 app.use(bodyParser.json()) //body for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) //body for parsing application/x-www-form-urlencoded
 
+var users = [
+    {id: 1, name:'Duc', phone: '098678965'},
+    {id: 2, name:'Tieu Vy', phone: '07898677'}
+]
+
+var books= [
+    {id: 1, title:'Muon Kiep Nhan Sinh', description: 'John Vu'},
+    {id: 2, title:'Tuong Lai The Gioi', description: 'Vo Dinh'}
+]
+
 app.get('/', (req, res) => {
     res.render('index', {
         name: 'Chào mừng bạn đến với thư viện sách Anh Đức'
@@ -18,19 +28,35 @@ app.get('/', (req, res) => {
 
 app.get('/users', function(req, res) {
     res.render('users/index',{
-        users: [
-            {id: 1, name:'Duc', phone: '098678965'},
-            {id: 2, name:'Tieu Vy', phone: '07898677'}
-        ]
+        users: users
+    })
+});
+
+app.get('/users/search', function(req, res) {
+    var q = req.query.q; // req.query là một object nên muốn lấy giá trị thì cần phải .p
+    console.log(req.query);
+    var matchedUser=users.filter(function(user){
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1
+    })
+    res.render('users/index',{
+        users: matchedUser
     })
 });
 
 app.get('/books', function(req, res) {
     res.render('books/index',{
-        books: [
-            {id: 1, title:'Muon Kiep Nhan Sinh', description: 'John Vu'},
-            {id: 2, title:'Tuong Lai The Gioi', description: 'Vo Dinh'}
-        ]
+        books: books
+    })
+});
+
+app.get('/books/search', function(req, res) {
+    var q = req.query.q; // req.query là một object nên muốn lấy giá trị thì cần phải .p
+    console.log(req.query);
+    var matchedBooks=books.filter(function(book){
+        return book.title.toLowerCase().indexOf(q.toLowerCase()) !== -1
+    })
+    res.render('books/index',{
+        books: matchedBooks
     })
 });
 
